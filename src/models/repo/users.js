7563b790525c8bdaa8d3723ad84d95bd596c8513
Walter Dal Mut <walter.dalmut@gmail.com>
2018-08-12 08:50:58 +0200

    Added objection.js as ORM support

--- src/models/repo/users.js
+++ src/models/repo/users.js
@@ -1,33 +1,32 @@
 const { assoc, ifElse, isNil, bind } = require('ramda');
 
-const config = require('config');
-const knex = require('knex')(config.db);
-
 const resolve = bind(Promise.resolve, Promise)
 const reject = bind(Promise.reject, Promise)
 const reject_because_already_exists = (user) => reject({status: 409, message: 'Already exists'})
 
+const User = require('../user');
+
 module.exports = {
   list: (params) => {
-    return knex('users');
+    return User.query();
   },
   get: (id) => {
-    return knex('users').where({id}).first();
+    return User.query().where({id}).first();
   },
   create: (body) => {
-    return knex('users').where({username: body.username}).first()
+    return User.query().where({username: body.username}).first()
       .then(ifElse(isNil, resolve, reject_because_already_exists))
-      .then(() => knex('users').insert(assoc('created_at', new Date(), body)))
-      .then((data) => knex('users').where({id: data.pop()}).first())
+      .then(() => User.query().insert(assoc('created_at', new Date(), body)))
+      .then((user) => User.query().where({id: user.id}).first())
     ;
   },
   update: (id, content) => {
     content = assoc('updated_at', new Date(), content);
 
-    return knex('users').where({ username: content.username }).whereNot({ id }).first()
+    return User.query().where({ username: content.username }).whereNot({ id }).first()
       .then(ifElse(isNil, resolve, reject_because_already_exists))
-      .then(() => knex('users').where({ id: id }).update(content))
-      .then(() => knex('users').where({id}).first())
+      .then(() => User.query().where({ id: id }).patch(content))
+      .then(() => User.query().where({id}).first())
     ;
   },
 };
---
+++ src/models/user.js
@@ -0,0 +1,32 @@
+const { Model } = require('objection');
+
+const config = require('config');
+const knex = require('knex')(config.db);
+
+Model.knex(knex);
+
+class User extends Model {
+  static get tableName() {
+    return 'users';
+  }
+
+  static get idColumn() {
+    return 'id';
+  }
+
+  static get jsonSchema () {
+    return {
+      type: 'object',
+      required: ['username', 'created_at'],
+
+      properties: {
+        id: { type: 'integer' },
+        username: { type: 'string' },
+        created_at: { type: 'datetime' },
+        updated_at: { type: 'datetime' },
+      },
+    }
+  }
+}
+
+module.exports = User;
