d06df6a07d3bddeff4b8ba5e940dde725937d56b
Walter Dal Mut <walter.dalmut@gmail.com>
2019-11-09 08:49:19 +0100

    Upgrate Joi dependency to v0.16

--- src/input-filters/product.js
+++ src/input-filters/product.js
@@ -4,39 +4,39 @@ const validator = require('express-joi-validation').createValidator({
 })
 const disable_convert = { joi: { convert: false, abortEarly: false } }
 
-exports.validate_products_input = validator.query({
+exports.validate_products_input = validator.query(Joi.object({
   limit: Joi.number().integer().min(1).max(1000),
-  page: Joi.number().integer().min(0).max(25),
+  page: Joi.number().integer().min(0),
   order: Joi.string().valid('ASC', 'DESC'),
   orderBy: Joi.string(),
   name: Joi.string().min(1),
   code: Joi.string().min(1),
   exit_date: Joi.date(),
-  status: Joi.string().valid(['ACTIVE', 'PENDING']),
+  status: Joi.string().valid('ACTIVE', 'PENDING'),
   available: Joi.boolean(),
-}, { joi: { abortEarly: false } })
+}), disable_convert)
 
-exports.validate_create_product_input = validator.body({
+exports.validate_create_product_input = validator.body(Joi.object({
   name: Joi.string().min(1).required(),
   code: Joi.string().min(1).required(),
   exit_date: Joi.date().options({ convert: true }).allow(null),
   available: Joi.boolean().allow(null),
   price: Joi.number().precision(8).allow(null),
-}, disable_convert)
+}), disable_convert)
 
-exports.validate_patch_product_role_admin_input = validator.body({
+exports.validate_patch_product_role_admin_input = validator.body(Joi.object({
   name: Joi.string().min(1),
   code: Joi.string().min(1),
   exit_date: Joi.date().allow(null),
-  status: Joi.string().valid(['ACTIVE', 'PENDING']),
+  status: Joi.string().valid('ACTIVE', 'PENDING'),
   available: Joi.boolean().allow(null),
   price: Joi.number().precision(8).allow(null),
-}, disable_convert)
+}), disable_convert)
 
-exports.validate_patch_product_role_user_input = validator.body({
+exports.validate_patch_product_role_user_input = validator.body(Joi.object({
   name: Joi.string().min(1),
   code: Joi.string().min(1),
   exit_date: Joi.date().allow(null),
   available: Joi.boolean().allow(null),
   price: Joi.number().precision(8).allow(null),
-}, disable_convert)
+}), disable_convert)
